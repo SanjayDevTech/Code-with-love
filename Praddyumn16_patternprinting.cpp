82e4c7975de72e7caf6200ccdefbb89ef7a5705b
Praddyumn Shukla <spraddyumn@gmail.com>
2020-10-01 12:42:48 +0530

    Update Praddyumn16_patternprinting.cpp

--- Praddyumn16_patternprinting.cpp
+++ Praddyumn16_patternprinting.cpp
@@ -22,32 +22,33 @@ int main(){
 	cin >> n; //taking a number as input, as in the above example, n=5
 
 
-	//printing the upper half of the rectangle
+	/*printing the upper half of the rectangle*/
   
 	for(int i = 0 ; i < n ; i++){
-		for(int j = 0 ; j < (2 * n) ; j++){
-			if(i + j <= n-1)  //upper left triangle
-				cout << "* ";
-			else
+	  for(int j = 0 ; j < (2 * n) ; j++){
+	    if(i + j <= n-1)  // upper left triangle
+		cout << "*";
+	    else
                 cout << " ";
             if((i + n) <= j)  // upper right triangle
-                cout << "* ";
+                cout << "*";
             else
                 cout << " ";
 		}
 		cout << "\n";
 	}
 
-	//printing the lower half of the rectangle
+	
+	/*printing the lower half of the rectangle*/
   
 	for(int i = 0 ; i < n ; i++){
-		for(int j = 0 ; j < (2 * n) ; j++){
-			if(i >= j)                // bottom left triangle
-                cout << "* ";
+	  for(int j = 0 ; j < (2 * n) ; j++){
+	    if(i >= j)                // bottom left triangle
+                cout << "*";
             else
                 cout << " ";
             if(i >= (2 * n - 1) - j)  // bottom right triangle
-                cout << "* ";
+                cout << "*";
             else
                 cout << " ";
 		}
