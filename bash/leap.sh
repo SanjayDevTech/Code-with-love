9f9cc071c7f35476d17d9bf4bcb4b2c793b952fd
calmcoconut <diaz.alejandro772@gmail.com>
2020-10-01 19:20:41 +0900

    Added Leap Year in bash

---
+++ bash/leap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+#
+# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+
+if [[ $# -eq 1 ]]
+then
+    if [[ (( $($1 % 4) == 0 )) ]]
+    then
+        echo yes
+        fi
+fi
