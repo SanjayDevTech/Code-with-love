2a83879f21ce4da90636ab9a78823dc810c81c64
Dhruv saini <37974516+DS-73@users.noreply.github.com>
2020-10-02 00:14:04 +0530

    Binary Search completed

--- cpp/DhruvSaini_binarysearch.cpp
+++ cpp/DhruvSaini_binarysearch.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Binary Search Function
 int binary(vector<int> v, int low, int high, int key){
     while(low <= high){
         int mid = low + ((high - low) / 2);
@@ -17,26 +18,32 @@ int binary(vector<int> v, int low, int high, int key){
             high = mid - 1;
         }
     }
+    // Returning -1 if element is not found
     return -1;
 }
 
 int main(){
     int n;
+    cout << "Enter the size : ";
     cin >> n;
 
     vector<int> v(n);
+    cout << "Enter elements[In ascending order] : ";
     for(int i = 0; i < n; i++){
         cin >> v[i];
     }
 
-    int m;
-    cin >> m;
-    vector<int> v1(m);
-    for(int i = 0; i < m; i++){
-        cin >> v1[i];
+    int key;
+    cout << "Enter key to be searched : ";
+    cin >> key;
+
+    // Searched Element
+    int res = binary(v, 0, n-1, key);
+    if(res == -1){
+        cout << "Key not found ";
     }
-    for(int i = 0; i < m; i++){
-        cout << binary(v, 0, n-1, v1[i]) << " ";
+    else{
+        cout << "Key found at index : " << res << endl;
     }
 
     return 0;
