4ab326e162fb715f24c78c19811a40b1215ed9b2
Mainak Debnath <debnath.mainak07@gmail.com>
2020-10-01 21:29:43 +0530

    Created solution for finding leap year or not in js

---
+++ js/mainakdebnath_leapYear.js
@@ -0,0 +1,13 @@
+// program to check leap year
+function checkLeapYear(year) {
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+let year = prompt('Enter a year:');
+
+checkLeapYear(year);
