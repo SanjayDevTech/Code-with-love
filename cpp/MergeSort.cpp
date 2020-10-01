6721f6c868e3b40bfa54a3ca87057f6c0b687a23
Siddhant Mittal <sidmit2001@gmail.com>
2020-10-01 13:24:37 +0530

    Very Efficient code for MergeSort

---
+++ cpp/MergeSort.cpp
@@ -0,0 +1,87 @@
+//ALGO FOR MERGE SORT
+/*
+Declare an array Arr of length N
+If N=1, Arr is already sorted
+If N>1,
+Left = 0, right = N-1
+Find middle = (left + right)/2
+Call merge_sort(Arr,left,middle) =>sort first half recursively
+Call merge_sort(Arr,middle+1,right) => sort second half recursively
+Call merge(Arr, left, middle, right) to merge sorted arrays in above steps.
+Exit
+*/
+
+//CODE
+#include <iostream>
+using namespace std;
+void merge(int *, int, int, int);
+void merge_sort(int *arr, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        //divide the array at mid and sort independently using merge sort
+        mid = (low + high) / 2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid + 1, high);
+        //merge or conquer sorted arrays
+        merge(arr, low, high, mid);
+    }
+}
+// Merge sort
+void merge(int *arr, int low, int high, int mid)
+{
+    int i, j, k, c[50];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            c[k] = arr[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            c[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        c[k] = arr[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        c[k] = arr[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)
+    {
+        arr[i] = c[i];
+    }
+}
+// read input array and call mergesort
+int main()
+{
+    int myarray[30], num;
+    cout << "Enter number of elements to be sorted:";
+    cin >> num;
+    cout << "Enter " << num << " elements to be sorted:";
+    for (int i = 0; i < num; i++)
+    {
+        cin >> myarray[i];
+    }
+    merge_sort(myarray, 0, num - 1);
+    cout << "Sorted array\n";
+    for (int i = 0; i < num; i++)
+    {
+        cout << myarray[i] << "\t";
+    }
+}
