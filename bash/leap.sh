08f7d4ae1b0fabfb615d9944b9f927b2ddf541e6
calmcoconut <diaz.alejandro772@gmail.com>
2020-10-02 09:15:44 +0900

    Added bash leap year

--- bash/leap.sh
+++ bash/leap.sh
@@ -1,32 +1,18 @@
-#!/usr/bin/env bash
-
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
-
-if [[ $# -eq 1 ]]
-then
-    if [[ (( $($1 % 4) == 0 )) ]]
+#!/bin/bash env
+function main() {
+regular_expr="^[0-9]+$"
+    if [[ $# -eq 1 && $1 =~ $regular_expr ]]
     then
-        echo yes
+        if ! (("$1" % 4)) && ( (("$1" % 100)) || ! (("$1" % 400)) )
+        then
+            echo true
+        else
+             echo false
         fi
-fi
+    else
+        echo 'Usage: leap.sh <year>'
+        return 1
+    fi
+}
+
+main "$@"
