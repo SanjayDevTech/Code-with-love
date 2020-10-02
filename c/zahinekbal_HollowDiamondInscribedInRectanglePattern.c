cbc48fdfe677934fe08f830bc5458fd268c93183
zahin ekbal <zahinekbal@users.noreply.github.com>
2020-10-02 20:33:24 +0530

    Added HollowDiamondInscribedRectanglePattern.C/C++/java by zahinekbal

---
+++ c/zahinekbal_HollowDiamondInscribedInRectanglePattern.c
@@ -0,0 +1,59 @@
+//Hollow Diamond Inscribed in a Rectangle Pattern
+
+
+/*   Input: 5
+
+Output:
+
+    * * * * * * * * * *
+    * * * *     * * * *
+    * * *         * * *
+    * *             * *
+    *                 *
+    *                 *
+    * *             * *
+    * * *         * * *
+    * * * *     * * * *
+    * * * * * * * * * *       
+
+ Solution for diamond inscribed inside rectangle In C :-  */
+
+ #include <stdio.h>
+int main()
+{
+    int i, j, n;
+    scanf("%d", &n);
+    // upper half of the pattern
+    for(i = 0; i < n; i++) 
+    {
+        for(j = 0; j < (2 * n); j++)
+        { 
+            if(i + j <= n - 1)  // upper left triangle
+                printf("*");
+            else
+                printf(" ");
+            if((i + n) <= j)  // upper right triangle
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    // bottom half of the pattern
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < (2 * n); j++)
+        {
+            if(i >= j)  //bottom left triangle
+                printf("*");
+            else
+                printf(" ");
+            if(i >= (2 * n - 1) - j)  // bottom right triangle
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
