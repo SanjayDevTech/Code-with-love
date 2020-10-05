9af0e090d91537458080411d3267fa3df7ae4e23
Samim Hossain Mondal <54748791+SamimMondal@users.noreply.github.com>
2020-10-05 11:52:52 +0530

    Update ASHISHAHUJA1904_BubbleSort.cpp
    Algorithm for optimized bubble sort 
    Time Complexity : O(n^2)

--- cpp/ASHISHAHUJA1904_BubbleSort.cpp
+++ cpp/ASHISHAHUJA1904_BubbleSort.cpp
@@ -1,33 +1,21 @@
-#include <iostream>
-using namespace std;
-
-int bubblesort(int array[],int size){
-    int temp,i,j;
-    for(i=0;i<size-1;i++){
-        for(j=0;j<size-i-1;j++){
-            if(array[j]>array[j+1]){
-                temp=array[j];
-                array[j]=array[j+1];
-                array[j+1]=temp;
-            }
-        }
-    }
-    return 0;
-} 
-int main()
+void bubbleSort(int *arr, int n)
 {
-    int array[]={1,34,56,78,89,23,83,28,90,37};
-    int size=sizeof(array)/sizeof(array[0]);
-    cout<<"Original Array is ";
-    for(int i=0;i<size;i++){
-        cout<<array[i]<<" ";
-    }
-    cout<<endl;
-    bubblesort(array,size);
-    cout<<"Sorted Array is ";
-    for(int i=0;i<size;i++){
-        cout<<array[i]<<" ";
-    }
-    return 0;
+    for(int i=0; i<n; i++)
+    {
+      bool flag = false;
+       for(int j=0; j<n-i-1; j++)
+       {
+          if(array[j]>array[j+1])
+          {
+            flag = true;
+             int temp = array[j+1];
+             array[j+1] = array[j];
+             array[j] = temp;
+          }
+       }
+      // No Swapping happened, array is sorted
+      if(!flag){
+         return;
+      }
+   }
 }
-  
