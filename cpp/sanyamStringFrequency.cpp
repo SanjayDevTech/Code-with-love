76afa689835f1f86a2afe8edb49633d49f7d7fb9
kittu-20 <sanyam.student.mec19@itbhu.ac.in>
2020-10-13 12:21:48 +0530

    Added the code for heap sort

--- cpp/sanyamStringFrequency.cpp
+++ cpp/sanyamStringFrequency.cpp
@@ -1,23 +1,10 @@
 #include <iostream> 
 using namespace std; 
-int count(string a, string b, int m, int n) 
-{ 
-    if ((m == 0 && n == 0) || n == 0) 
-        return 1; 
-    if (m == 0) 
-        return 0; 
-    if (a[m - 1] == b[n - 1]) 
-        return count(a, b, m - 1, n - 1) + 
-               count(a, b, m - 1, n); 
-    else
-        return count(a, b, m - 1, n); 
-} 
+
 int main() 
 { 
-    string a = "abcabcadrewabc"; 
-    string b = "abc"; 
   
-    cout << count(a, b, a.size(), b.size()) << endl; 
+    cout <<"ssanyam"; 
   
     return 0; 
 } 
